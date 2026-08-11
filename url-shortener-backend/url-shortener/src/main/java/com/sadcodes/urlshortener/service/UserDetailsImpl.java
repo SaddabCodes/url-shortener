426400323d55897958f47411e4905e1219cdd709
saddab <saddabakhtar17@gmail.com>
2026-08-11 15:56:31 +0530

    feat(backend): add UserDetailsImpl for Spring Security integration

---
+++ url-shortener-backend/url-shortener/src/main/java/com/sadcodes/urlshortener/service/UserDetailsImpl.java
@@ -0,0 +1,51 @@
+package com.sadcodes.urlshortener.service;
+
+import com.sadcodes.urlshortener.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+
+    private static final Long serialVersionUID = 1L;
+
+    private Long id;
+    private String username;
+    private String email;
+    private String password;
+
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserDetailsImpl build(User user) {
+        GrantedAuthority authority = new SimpleGrantedAuthority(user.getRole());
+        return new UserDetailsImpl(user.getId(), user.getUsername(), user.getEmail(), user.getPassword(), Collections.singletonList(authority));
+    }
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public @Nullable String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}
