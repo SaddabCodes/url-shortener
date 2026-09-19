0ef3c78e1cd4187c0869600d411fc98ba5761e9d
saddab <saddabakhtar17@gmail.com>
2026-09-19 16:20:40 +0530

    feat(backend): implement redirect controller with click tracking

---
+++ url-shortener-backend/url-shortener/src/main/java/com/sadcodes/urlshortener/controller/RedirectController.java
@@ -0,0 +1,29 @@
+package com.sadcodes.urlshortener.controller;
+
+import com.sadcodes.urlshortener.model.UrlMapping;
+import com.sadcodes.urlshortener.services.UrlMappingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class RedirectController {
+
+    private final UrlMappingService urlMappingService;
+
+    @GetMapping("/{shortUrl}")
+    public ResponseEntity<Void> redirect(@PathVariable String shortUrl) {
+        UrlMapping urlMapping = urlMappingService.getOriginalUrl(shortUrl);
+        if (urlMapping != null) {
+            HttpHeaders httpHeaders = new HttpHeaders();
+            httpHeaders.add("Location",urlMapping.getOriginalUrl());
+            return ResponseEntity.status(302).headers(httpHeaders).build();
+        }else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
